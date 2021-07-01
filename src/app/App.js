d0d4f1b7a06ced6db65de418df71d3094861780d
Alan <afudge@usaviationacademy.com>
2021-07-01 17:54:20 -0500

    implement basic routing for app

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import FormList from '../features/forms/formList';
+import Form from '../features/forms/form';
+import NewSectionForm from '../components/newSectionForm';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 export default function App() {
     return (
-        <>
-            <h1>Here is the list of forms</h1>
-            <FormList />
-        </>
+        <Router>
+            <Switch>
+                <Route path='/forms/:formId'>
+                    <h1>Here is the form</h1>
+                    <Form />
+                </Route>
+                <Route path='/forms'>
+                    <h1>Here is the list of forms</h1>
+                    <FormList />
+                </Route>
+                <Route path='/'>
+                    <Redirect to='/forms' />
+                </Route>
+            </Switch>
+        </Router>
     );
 }
