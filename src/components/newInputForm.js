6c1a826f9756b964839ba3fdfeb7a04e02215578
Alan <afudge@usaviationacademy.com>
2021-07-07 15:31:51 -0500

    add attributes section to new input form along with complete type selection; replace attributes object with individual attribute states

--- src/components/newInputForm.js
+++ src/components/newInputForm.js
@@ -7,24 +7,49 @@ export default function NewInputForm({ sectionId }) {
     const [title, setTitle] = useState('');
     const [name, setName] = useState('');
     const [type, setType] = useState('text');
-    const [attributes, setAttributes] = useState([]);
+    const [required, setRequired] = useState(false)
+    const [min, setMin] = useState(null);
+    const [max, setMax] = useState(null);
+    const [pattern, setPattern] = useState(null);
+    const [maxlength, setMaxlength] = useState(null);
     const dispatch = useDispatch();
+    const [selectOptions, setSelectOptions] = useState([]);
+    
+    const handleSelectOptionsChange = (e) => {
+        e.preventDefault();
+
+        setSelectOptions(e.currentTarget.value.split(','));
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
         dispatch(addInputThunk({
             sectionId,
-            input:{
+            input: {
                 inputId: uuidv4(),
                 name,
                 title,
                 type,
-                attributes
+                attributes: {
+                    min,
+                    max,
+                    pattern,
+                    maxlength,
+                    required
+                },
+                selectOptions
             }
         }))
 
-        console.log('submitted');
+        setTitle('');
+        setName('');
+        setType('text');
+        setRequired(false);
+        setMin(null);
+        setMax(null);
+        setPattern(null);
+        setMaxlength(null);
     }
 
     return (
@@ -34,7 +59,37 @@ export default function NewInputForm({ sectionId }) {
             <select value={type} onChange={e => setType(e.currentTarget.value)}>
                 <option value='text'>text</option>
                 <option value='number'>number</option>
+                <option value='color'>color</option>
+                <option value='date'>date</option>
+                <option value='datetime-local'>datetime-local</option>
+                <option value='email'>email</option>
+                <option value='file'>file</option>
+                <option value='image'>image</option>
+                <option value='month'>month</option>
+                <option value='tel'>telephone</option>
+                <option value='time'>time</option>
+                <option value='url'>url</option>
+                <option value='week'>week</option>
+                <option value='select'>select</option>
             </select>
+            {
+                type === 'select' ?
+                    <input type='text' value={selectOptions.join(',')} onChange={handleSelectOptionsChange} placeholder='comma seperated list of select options'></input>
+                    : null
+            }
+            <div class='attributes-list'>
+                <p>Leave empty for no attribute</p>
+                <label htmlFor='required'>Required?</label>
+                <input name='required' type='checkbox' checked={required} onChange={e => setRequired(!required)}></input>
+                <label htmlFor='min'>Min</label>
+                <input name='min' type='number' value={min} onChange={e => e.currentTarget.value === '' ? setMin(null) : setMin(e.currentTarget.value)}></input>
+                <label htmlFor='max'>Max</label>
+                <input name='max' type='number' value={max} onChange={e => e.currentTarget.value === '' ? setMax(null) : setMax(e.currentTarget.value)}></input>
+                <label htmlFor='pattern'>RegEx Pattern</label>
+                <input name='pattern' type='text' placeholder='/[a-z]/gi' value={pattern} onChange={e => e.currentTarget.value === '' ? setPattern(null) : setPattern(e.currentTarget.value)}></input>
+                <label htmlFor='maxlength'>Max Length</label>
+                <input name='max-length' type='number' placeholder='max length of text' value={maxlength} onChange={e => e.currentTarget.value === '' ? setMaxlength(null) : setMaxlength(e.currentTarget.value)}></input>
+            </div>
             <input type='submit' value='Add New Input'></input>
         </form>
     )
