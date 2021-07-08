1b5ad2b5bfb34237a5f80c5b1e9c28d85735ccac
Alan <afudge@usaviationacademy.com>
2021-07-08 16:16:11 -0500

    remove excess attributes from input state

--- stateExample.js
+++ stateExample.js
@@ -23,14 +23,11 @@ const exampleState = {
             name: "input-name-no-spaces",
             type: "some-html-input-type",
             attributes: {
-                checked: "default-undefined",
-                disabled: "default-undefined",
+                required: "default-undefined",
                 max: "default-undefined",
-                maxlength: "default-undefined",
                 min: "default-undefined",
                 pattern: "default-undefined",
-                readonly: "default-undefined",
-                required: "default-undefined",
+                maxlength: "default-undefined",
                 step: "default-undefined",
                 placeholder: "default-undefined",
 
