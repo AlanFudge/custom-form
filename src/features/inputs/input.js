04978ff616390a175d9902ef3d39fbefc3d9b06b
Alan <afudge@usaviationacademy.com>
2021-07-09 15:47:41 -0500

    add ability to swap index up and down the list

--- src/features/inputs/input.js
+++ src/features/inputs/input.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import NewInputForm from '../../components/newInputForm';
+import { selectSections, swapInputIndexes } from '../sections/sectionsSlice';
 import { deleteInputThunk, selectInputs } from './inputsSlice';
 
 export default function Input({ inputId, sectionId }) {
     const [edit, setEdit] = useState(false);
     const inputs = useSelector(selectInputs);
+    const sections = useSelector(selectSections);
     const input = inputs[inputId];
+    const section = sections[sectionId];
     const dispatch = useDispatch();
 
     const deleteInputHandler = (e) => {
@@ -17,13 +20,51 @@ export default function Input({ inputId, sectionId }) {
         }));
     }
 
+    const handleSwapUp = (e) => {
+        const currentIndex = section.inputs.indexOf(input.inputId);
+        if (currentIndex <= 0) {
+            e.preventDefault();
+        } else {
+            e.preventDefault();
+
+            const input1Id = input.inputId;
+            const input2Id = section.inputs[currentIndex - 1];
+
+            dispatch(swapInputIndexes({
+                sectionId,
+                input1Id,
+                input2Id
+            }))
+        }
+    }
+
+    const handleSwapDown = (e) => {
+        const currentIndex = section.inputs.indexOf(input.inputId);
+        if (currentIndex < 0 || currentIndex == sections[sectionId].inputs.length - 1) {
+            e.preventDefault();
+        } else {
+            e.preventDefault();
+
+            const input1Id = input.inputId;
+            const input2Id = section.inputs[currentIndex + 1];
+
+            dispatch(swapInputIndexes({
+                sectionId,
+                input1Id,
+                input2Id
+            }))
+        }
+    }
+
     return (
         <>
             {
                 edit ?
-                    <NewInputForm edit={edit} setEdit={setEdit} input={input} />
+                    <NewInputForm sectionId={sectionId} edit={edit} setEdit={setEdit} input={input} />
                     :
                     <div>
+                        <button className='swap-up' onClick={handleSwapUp}>↑</button>
+                        <button className='swap-down' onClick={handleSwapDown}>↓</button>
                         <h3>{input.inputId}</h3>
                         <h4>{input.name}</h4>
                         <h5>{input.title}</h5>
