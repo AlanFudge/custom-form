4a3400e8dc5473ea2338e52b6a1d4a5cc363e0cc
Alan <afudge@usaviationacademy.com>
2021-07-14 14:49:41 -0500

    add select and radio types to component

--- src/components/displayInput.js
+++ src/components/displayInput.js
@@ -4,11 +4,44 @@ import { selectInputs } from '../features/inputs/inputsSlice';
 
 export default function DisplayInput({ inputId }) {
     const inputs = useSelector(selectInputs);
-    const { name, type, title, attributes } = inputs[inputId];
+    const { name, type, title, attributes, selectOptions } = inputs[inputId];
     const inputAttributes = {
         name,
         type,
         ...attributes
+    }
+
+    if (type === 'select') {
+        return (
+            <div id={inputId}>
+                <label htmlFor={name}>{title}</label>
+                <select {...inputAttributes}>
+                    {
+                        selectOptions.map(value => {
+                            return <option value={value}>{value}</option>
+                        })
+                    }
+                </select>
+            </div>
+        )
+    } else if (type === 'radio') {
+        return (
+            <div id={inputId}>
+                <label htmlFor={name}>{title}</label>
+                <br />
+                {
+                    selectOptions.map(value => {
+                        return (
+                            <>
+                                <input {...inputAttributes} value={value}></input>
+                                <label htmlFor={value}>{value}</label>
+                                <br />
+                            </>
+                        )
+                    })
+                }
+            </div>
+        )
     }
 
     return (
